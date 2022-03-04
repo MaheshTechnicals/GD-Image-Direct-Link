da82a0fd56fecb247743fcee556f83f2fb9b018e
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-03-04 22:39:28 +0530

    New branch added!

---
+++ main.js
@@ -0,0 +1,15 @@
+var btn = document.getElementById("getLink");
+var link = "https://drive.google.com/uc?id=";
+
+var output = document.getElementById("output");
+
+btn.addEventListener("click", function() {
+  var your = document.getElementById("yourId").value;
+  let a = your.slice(32, your.length);
+  var removeAfter = a.indexOf("/");
+  var final = a.substring(0, removeAfter);
+  var fullLink = link + final;
+
+  output.innerHTML = `<a id="btn2" href ="${fullLink}">${fullLink}</a>`;
+
+})
