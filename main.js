08792f0a5132dc30b59fb0642b0e72c67c434ab6
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-03-05 16:16:30 +0530

    Some cleanups

--- main.js
+++ main.js
@@ -5,28 +5,22 @@ var output = document.getElementById("output");
 
 btn.addEventListener("click", function() {
   var your = document.getElementById("yourId").value;
- /* let a = your.slice(32, your.length);
-  var removeAfter = a.indexOf("/");
-  var final = a.substring(0, removeAfter);
-  var fullLink = link + final;
 
-  output.innerHTML = `<a id="btn2" href ="${fullLink}">${fullLink}</a>`;
-*/
 
-if (your.length<=33) {
-  var id =link+your;
-  output.innerHTML = `<a id="btn2" href ="${id}">${id}</a>`;
- 
-  
-} else {
-  
-  let a = your.slice(32, your.length);
-  var removeAfter = a.indexOf("/");
-  var final = a.substring(0, removeAfter);
-  var fullLink = link + final;
-  
-  output.innerHTML = `<a id="btn2" href ="${fullLink}">${fullLink}</a>`;
+  if (your.length <= 33) {
+    var id = link + your;
+    output.innerHTML = `<a id="btn2" href ="${id}">${id}</a>`;
 
-}
+
+  } else {
+
+    let a = your.slice(32, your.length);
+    var removeAfter = a.indexOf("/");
+    var final = a.substring(0, removeAfter);
+    var fullLink = link + final;
+
+    output.innerHTML = `<a id="btn2" href ="${fullLink}">${fullLink}</a>`;
+
+  }
 
 })
